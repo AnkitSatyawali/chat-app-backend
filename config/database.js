13621ddc7fe903796b51ee63c9c2cf7df007661f
Ankit Satyawali <ankitsatywali@gmail.com>
2020-07-08 23:33:02 +0530

    code pushed

---
+++ config/database.js
@@ -0,0 +1,8 @@
+module.exports = {
+   database:"mongodb+srv://develop:hI1DeIJl36cArQTh@cluster0.bchr2.mongodb.net/test?retryWrites=true&w=majority",
+   JWT_KEY : "mychatproject",
+   clientID: "58d2ec94ff7ad976e6c2",
+   clientSecret:"973e3c110b726d0e05f988d9e838eb277a581721"
+}
+
+// database:"mongodb+srv://developer:Nv6O9n07Br6C3IT9@cluster0-1hybp.mongodb.net/test?retryWrites=true&w=majority", 
